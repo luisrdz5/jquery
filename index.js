10e87f4bf2ae855262b7e8a2c7fa6391d6e8e225
lrodriguez <luis.rodriguez22@cfe.gob.mx>
2016-03-30 20:39:31 -0600

    Se agrego ajax al javascript

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
 
 $(function() {
-	var a=$('<a>',{
-		href: 'http://platzi.com',
-		target: '_blank',
-		html: 'Ir a Platzi'
+	var tvshows = $('#app-body').find('.tv-shows')
+	function renderShows(shows){
+		shows.forEach(function(show){
+			var article=template 
+				.replace(':name:',show.name)
+				.replace(':img:',show.image.medium)
+				.replace(':summary:',show.summary)
+				.replace(':img alt:',show.name + " Logo")
+			var $article = $(article);
+			$article.hide();
+			tvshows.append($article.show());
+			})
+	}
+	/*
+	*	Submit search 
+	*/
+
+	$('app-body')
+		.find('form')
+		.submit(function (ev){
+			ev.preventDefault();
+			var busqueda = $(this)
+				.find('input[type="text"]')
+				.val();
+			$tvShowsContainer.find('.tv-show').remove()
+			$('<div class="loader">').appendTo($tvShowsContainer)
+			$.ajax({
+				url: 'http://api.tvmaze.com/search/shows',
+				data: { q: busqueda },
+				success: function (res, textStatus, xhr){
+					$loader.remove();
+					var shows = res.map(function (el){
+						return el.show
+					})
+					renderShows(shows);
+				}
+			})
+		})
+	var template='<article class="tv-show">' + 
+	'<div class="left img-container">' + 
+	'<img src=":img:" alt=":imgalt:" />'+
+	'</div>'+
+	'<div class="right info">'+
+	'<h1> :name: </h1>'+
+	'<p> :summary: </p>'+
+	'</div>'+
+	'</article>';
+
+
+	$.ajax({
+		url: 'http://api.tvmaze.com/shows',
+		success: function(shows,textStatus,xhr){	
+			tvshows.find('.loader').remove();
+			renderShows(shows);
+		}		
 	})
-	$('#app-body').append(a);
-
-
-
 })
